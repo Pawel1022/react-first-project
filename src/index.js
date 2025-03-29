61ecaedb858f1af27eac81b70c311b37c64845ba
Pawel1022 <Pawel.wozniak200116@gmail.com>
2025-03-29 19:14:46 +0100

    added rendering list

--- src/index.js
+++ src/index.js
@@ -2,71 +2,95 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-	<React.StrictMode>
-		<Card />
-	</React.StrictMode>
-)
+const skills = [
+	{
+		id: 1,
+		skill: 'HTML + CSS',
+		emoji: '💪',
+		bgColor: 'lime',
+	},
+	{
+		id: 2,
+		skill: 'SASS',
+		emoji: '💪',
+		bgColor: 'purple',
+	},
+	{
+		id: 3,
+		skill: 'JavaScript',
+		emoji: '💪',
+		bgColor: 'red',
+	},
+	{
+		id: 4,
+		skill: 'GIT & Github',
+		emoji: '😊',
+		bgColor: 'yellow',
+	},
+	{
+		id: 5,
+		skill: 'WEB DESIGN',
+		emoji: '😊',
+		bgColor: 'lightseagreen',
+	},
+	{
+		id: 6,
+		skill: 'React',
+		emoji: '📘',
+		bgColor: 'deeppink',
+	},
+]
 
-function Card() {
+function App() {
 	return (
 		<figure className='card'>
-			<Avatar path='logo512.png' alt='React Logo' />
-			<CardBody />
+			<Avatar path='logo512.png' title='React LOGO' />
+			<Overview />
 		</figure>
 	)
 }
 
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
+
 function Avatar(props) {
-	console.log(props)
-	return <img src={props.path} alt={props.alt} className='avatar'></img>
+	return <img alt={props.title} src={props.path} className='avatar'></img>
 }
 
-function CardBody() {
+function Overview() {
 	return (
 		<div className='data'>
-			<Fullname name='Paweł Woźniak' />
-			<Biography />
-			<Skills />
+			<h1>Paweł Woźniak</h1>
+			<p>
+				I am a novice programmer and computer science student who is actively developing my skills in creating websites
+				and web applications. I am currently learning front-end technologies, including React.js, and building my
+				portfolio of projects.
+			</p>
+			<SkillList />
 		</div>
 	)
 }
 
-function Fullname(props) {
-	return <h1>{props.name}</h1>
-}
-
-function Biography() {
-	return (
-		<p>
-			I am a novice programmer and computer science student who is actively developing my skills in creating websites
-			and web applications. I am currently learning front-end technologies, including React.js, and building my
-			portfolio of projects.
-		</p>
-	)
-}
-
-function Skills() {
+function SkillList() {
 	return (
 		<ul className='skill-list'>
-			<Skill skill='HTML + CSS' emoji='💪' bgColor='lime' />
-			<Skill skill='SASS' emoji='💪' bgColor='purple' />
-			<Skill skill='JavaScript' emoji='💪' bgColor='red' />
-			<Skill skill='GIT & Github' emoji='😊' bgColor='yellow' />
-			<Skill skill='WEB DESIGN' emoji='😊' bgColor='lightseagreen' />
-			<Skill skill='React' emoji='📘' bgColor='deeppink' />
+			{skills.map(data => (
+				<Skill skillObj={data} key={data.id} />
+			))}
 		</ul>
 	)
 }
 
 function Skill(props) {
-	const color = {
-		backgroundColor: props.bgColor,
-	}
+	const data = props.skillObj
+	const dataStyle = { backgroundColor: data.bgColor }
 	return (
-		<li style={color} className='skill'>
-			{props.skill} {props.emoji}
+		<li className='skill' style={dataStyle}>
+			{data.skill} {data.emoji}
 		</li>
 	)
 }
