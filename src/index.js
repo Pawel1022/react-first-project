d88f78014e055d3d806860eb964227a91397ac72
Pawel1022 <Pawel.wozniak200116@gmail.com>
2025-03-30 12:54:25 +0200

    rebuild

--- src/index.js
+++ src/index.js
@@ -2,54 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 
-const skills = [
-	{
-		id: 1,
-		skill: 'HTML + CSS',
-		emoji: '💪',
-		bgColor: 'lime',
-	},
-	{
-		id: 2,
-		skill: 'SASS',
-		emoji: '💪',
-		bgColor: 'purple',
-	},
-	{
-		id: 3,
-		skill: 'JavaScript',
-		emoji: '💪',
-		bgColor: 'red',
-	},
-	{
-		id: 4,
-		skill: 'GIT & Github',
-		emoji: '😊',
-		bgColor: 'yellow',
-	},
-	{
-		id: 5,
-		skill: 'WEB DESIGN',
-		emoji: '😊',
-		bgColor: 'lightseagreen',
-	},
-	{
-		id: 6,
-		skill: 'React',
-		emoji: '📘',
-		bgColor: 'deeppink',
-	},
-]
-
-function App() {
-	return (
-		<figure className='card'>
-			<Avatar path='logo512.png' title='React LOGO' />
-			<Overview />
-		</figure>
-	)
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
@@ -57,40 +9,101 @@ root.render(
 	</React.StrictMode>
 )
 
-function Avatar(props) {
-	return <img alt={props.title} src={props.path} className='avatar'></img>
-}
-
-function Overview() {
-	return (
-		<div className='data'>
-			<h1>Paweł Woźniak</h1>
-			<p>
-				I am a novice programmer and computer science student who is actively developing my skills in creating websites
+const personalData = {
+	fullname: 'Paweł Woźniak',
+	biography: `I am a novice programmer and computer science student who is actively developing my skills in creating websites
 				and web applications. I am currently learning front-end technologies, including React.js, and building my
-				portfolio of projects.
-			</p>
-			<SkillList />
+				portfolio of projects.`,
+	skills: [
+		{
+			id: 1,
+			skill: 'HTML + CSS',
+			level: 'advanced',
+			cssStyles: {
+				backgroundColor: '#00FF00',
+			},
+		},
+		{
+			id: 2,
+			skill: 'SASS',
+			level: 'advanced',
+			cssStyles: {
+				backgroundColor: '#800080',
+			},
+		},
+		{
+			id: 3,
+			skill: 'JavaScript',
+			level: 'intermediate',
+			cssStyles: {
+				backgroundColor: '#FF0000',
+			},
+		},
+		{
+			id: 4,
+			skill: 'GIT & Github',
+			level: 'advanced',
+			cssStyles: {
+				backgroundColor: '#FFFF00',
+			},
+		},
+		{
+			id: 5,
+			skill: 'WEB DESIGN',
+			level: 'advanced',
+			cssStyles: {
+				backgroundColor: '#20B2AA',
+			},
+		},
+		{
+			id: 6,
+			skill: 'React',
+			level: 'begginer',
+			cssStyles: {
+				backgroundColor: '#FF1493',
+			},
+		},
+	],
+}
+function App() {
+	return (
+		<div className='container'>
+			<figure className='card'>
+				<Avatar path={'/logo512.png'} altText={'React LOGO'} />
+				<Overview data={personalData} />
+			</figure>
 		</div>
 	)
 }
 
-function SkillList() {
+function Avatar({ path, altText }) {
+	return <img className='avatar' src={path} alt={altText}></img>
+}
+
+function Overview({ data }) {
+	return (
+		<div className='data'>
+			<h1>{data.fullname}</h1>
+			<p>{data.biography}</p>
+			<SkillList skills={data.skills} />
+		</div>
+	)
+}
+
+function SkillList({ skills }) {
 	return (
 		<ul className='skill-list'>
-			{skills.map(data => (
-				<Skill skillObj={data} key={data.id} />
+			{skills.map(skill => (
+				<Skill data={skill} key={skill.id} />
 			))}
 		</ul>
 	)
 }
 
-function Skill(props) {
-	const data = props.skillObj
-	const dataStyle = { backgroundColor: data.bgColor }
+function Skill({ data }) {
 	return (
-		<li className='skill' style={dataStyle}>
-			{data.skill} {data.emoji}
+		<li className='skill' style={data.cssStyles}>
+			{data.skill} {data.level === 'advanced' ? '💪' : '😊'}
 		</li>
 	)
 }
